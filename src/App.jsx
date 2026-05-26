a06fa6cfca53ac389fde5320d0cee67f8a5ca480
agiv <webdev111222@gmail.com>
2026-05-26 14:56:46 -0700

    finished step 2 on tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
 import Header from './Header';
+import { useState } from 'react';
 import Footer from './Footer';
 import Explorer from './Explorer';
+import axios from 'axios';
+import'bootstrap/dist/css/bootstrap.min.css';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  // const [searchQuery, setSearchQuery] = useState('')
-  // const [location, setLocation] = useState({});
+  const [searchQuery, setSearchQuery] = useState('')
+  const [location, setLocation] = useState({});
 
-  // async function getLocation() {
-  //   const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
-  //   const response = await axios.get(API);
-  //   setLocation(response.data[0]);
-  // }
-
+   async function getLocation() {
+     const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
+    const response = await axios.get(API);
+      setLocation(response.data[0]);
+      
   return (
-    <> </>
-    <div className="App">
+    <> 
+    <input
+      onChange={(e) => setSearchQuery(e.target.value)}
+    placeholder="Search for City"
+    />
+    <button onClick={getLocation}>
+      Explore!
+    </button>
+
+    {location.place_id && (
+ <div>
+   <h2>{location.display_name}</h2>
+ 
+   <p>Latitude: {location.lat}</p>
+ 
+   <p>Longitude: {location.lon}</p>
+ </div>
+       )
+    }
+
+  <div className="card p-3 mt-3">
+    <h2>{location.display_name}</h2>
+ 
+    <p>Latitude: {location.lat}</p>
+ 
+    <p>Longitude: {location.lon}</p>
+</div> 
+   
+    <div 
+    className="App">
       <Header />
       <Explorer />
       <Footer />
