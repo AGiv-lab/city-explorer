5f722b8fcf6bc1b51fb639b38ed4cd0e48295cd9
agiv <webdev111222@gmail.com>
2026-05-28 15:36:09 -0700

    added map

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import { useState } from 'react';
-import axios from 'axios';
+import axios from 'axios'; //imports the axios library for making HTTP requests
 import 'bootstrap/dist/css/bootstrap.min.css';
  
+ 
+// Importing API key
 const API_KEY = import.meta.env.VITE_API_KEY;
  
 function App() {
- 
+// States and setStates for search query and location data
   const [searchQuery, setSearchQuery] = useState('');
   const [location, setLocation] = useState({});
  
   async function getLocation() {
  
-    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
+      const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
  
-    const response = await axios.get(API);
+      const response = await axios.get(API);
  
-    setLocation(response.data[0]);
+      setLocation(response.data[0]);
   }
+ 
+  const mapUrl =
+    location.lat && location.lon
+      ? `https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=10`
+      : '';
  
   return (
     <div className="container mt-5">
@@ -29,13 +36,18 @@ function App() {
  
       <button
         className="btn btn-primary mt-3"
-        onClick={getLocation}
-      >
+        onClick={getLocation}>
         Explore!
       </button>
  
       {location.place_id && (
         <div className="card p-3 mt-3">
+ 
+          <img
+            src={mapUrl}
+            alt="Map of city"
+          />
+ 
  
           <h2>{location.display_name}</h2>
  
@@ -51,4 +63,3 @@ function App() {
 }
  
 export default App;
- 
