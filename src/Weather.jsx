8116129c11fd3eb2a05ad64afb7d3b2d6cb9d94b
agiv <webdev111222@gmail.com>
2026-06-16 15:45:04 -0700

    weather render

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,12 +1,12 @@
 function Weather(props) {
   return (
-    <section>
+    <section className="mt-3">
       <h2>Weather</h2>
 
       {props.weather.map((day, index) => (
-        <div key={index}>
-          <p>Day: {day.date}</p>
-          <p>Description: {day.description}</p>
+        <div className="card p-3 mb-2" key={index}>
+          <p><strong>Date:</strong> {day.date}</p>
+          <p><strong>Description:</strong> {day.description}</p>
         </div>
       ))}
     </section>
